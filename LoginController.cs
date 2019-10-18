7d172c0c27ccd0ed0fc5ad58debe7aa5f888bd65
PotegoLudwick <50365291+PotegoLudwick@users.noreply.github.com>
2019-10-18 18:14:52 +0200

    Add files via upload
    Updated login , all clients under one entity

--- LoginController.cs
+++ LoginController.cs
@@ -17,75 +17,92 @@ namespace Deliverable_2_WireFrames.Controllers
         {
             return View();
         }
-    
 
 
-public ActionResult Create()
-{
-    ViewBag.UserType = new SelectList(db.UserTypes, "ID", "Description");
-    return View();
-}
 
-[HttpPost]
-public ActionResult Create(User user)
-{
-
-    if (ModelState.IsValid)
-    {
-        var hashedpassword = ComputeSha256Hash(user.UserPassword);
-        Models.User NewUser = new User();
-        NewUser.UserEmail = user.UserEmail;
-        NewUser.UserPassword = hashedpassword;
-        NewUser.UserType = user.UserType;
-        db.Users.Add(NewUser);
-        db.SaveChanges();
-        return RedirectToAction("Index", "Homepage");
-    }
-
-    ViewBag.UserType = new SelectList(db.UserTypes, "ID", "Description", user.UserType);
-    return View(user);
-}
-
-OnlinePharmacyEntities db = new OnlinePharmacyEntities();
-[HttpPost]
-
-public ActionResult Login(string Username, string Password, User user)
-{
-
-    var hashedpassword = ComputeSha256Hash(Password);
-
-    user = db.Users.Where(zz => zz.UserEmail == Username
-                                      && zz.UserPassword == hashedpassword)
-                                      .FirstOrDefault();
-    if (user != null)
-    {
-        UserVM userVME = new UserVM();
-        userVME.user = user;
-        userVME.RefreshGUID(db);
-        TempData["userVM"] = userVME;
-        return RedirectToAction("Index", "Homepage");
-
-    }
-
-    return RedirectToAction("Index", "Home", "Error");
-}
-
-string ComputeSha256Hash(string Password)
-{
-    using (SHA256 sha256Hash = SHA256.Create()) //Create a SHA256
-    {
-        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(Password)); // return byte array
-
-        //convert byte array into string
-        StringBuilder builder = new StringBuilder();
-        for (int x = 0; x < bytes.Length; x++)
+        public ActionResult Create()
         {
-            builder.Append(bytes[x].ToString("x2"));
+            ViewBag.UserType = new SelectList(db.UserTypes, "UserTypeID", "Description");
+            ViewBag.VillageID = new SelectList(db.Villages, "Village_ID", "Village_Name");
+            return View();
         }
-        return builder.ToString();
-    }
 
-}
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var hashedpassword = ComputeSha256Hash(user.UserPassword);
+                    Models.User NewUser = new User();
+                    NewUser.Name = user.Name;
+                    NewUser.Surname = user.Surname;
+                    NewUser.PhoneNumber = user.PhoneNumber;
+                    NewUser.Gender = user.Gender;                 
+                    NewUser.Village = user.Village;
+                    NewUser.UserEmail = user.UserEmail;
+                    NewUser.UserPassword = hashedpassword;
+                    NewUser.UserType = user.UserType;
+                    db.Users.Add(NewUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Homepage");
+                }
+            }
+
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+            ViewBag.UserType = new SelectList(db.UserTypes, "UserTypeID", "Description", user.UserType);
+            ViewBag.VillageID = new SelectList(db.Villages, "Village_ID", "Village_Name", user.Village_ID);
+            return View(user);
+        }
+
+        OnlinePharmacyEntities db = new OnlinePharmacyEntities();
+        [HttpPost]
+
+        public ActionResult Login(string Username, string Password, User user)
+        {
+
+            var hashedpassword = ComputeSha256Hash(Password);
+
+            user = db.Users.Where(zz => zz.UserEmail == Username
+                                              && zz.UserPassword == hashedpassword)
+                                              .FirstOrDefault();
+            if (user != null)
+            {
+                UserVM userVME = new UserVM();
+                userVME.user = user;
+                userVME.RefreshGUID(db);
+                TempData["userVM"] = userVME;
+                return RedirectToAction("Index", "Homepage");
+
+            }
+
+            return RedirectToAction("Index", "Home", "Error");
+        }
+
+        string ComputeSha256Hash(string Password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create()) //Create a SHA256
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(Password)); // return byte array
+
+                //convert byte array into string
+                StringBuilder builder = new StringBuilder();
+                for (int x = 0; x < bytes.Length; x++)
+                {
+                    builder.Append(bytes[x].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
-  
+
+
+
+
+
+
